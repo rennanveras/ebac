3d0912c907c9e7f918d1ec7a56d9429c13b68d60
rennan veras <112007290+rennanveras@users.noreply.github.com>
2023-07-06 18:33:41 -0300

    Delete src directory

--- src/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function multiplica(valor1, valor2) {
-    return (valor1 * valor2);
-}
-function dizOla(nome) {
-    return `Olá ${nome}`;
-}
-console.log(multiplica(2, 5));
-console.log(dizOla('Rennan'));
